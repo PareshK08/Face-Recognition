cad9f630636b2e4134cf3e560a631ba609068d86
Paresh Kulkarni <kulkarniparesh45@gmail.com>
2025-02-01 15:48:33 +0530

    Add files via upload
    I am adding this program to check test images . Is haar_face detecting face properly or not . Check your test image with this program .
    If face is not detected then try with resizing the image .

---
+++ face_detect.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+img =cv.imread(r'D:\Img\Train\Sachin Tendulkar\25.jpg')
+img = cv.resize(img,(500,500))
+#cv.imshow('Group',img)
+
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+#cv.imshow('Gray',gray)
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+face_rect = haar_cascade.detectMultiScale(gray,
+                scaleFactor=1.4,minNeighbors=4)
+
+print(f'No. of faces : {len(face_rect)}')
+
+for (x,y,w,h) in face_rect:
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
+    
+
+cv.imshow('Detected Face',img)
+
+
+
+cv.waitKey(0)
